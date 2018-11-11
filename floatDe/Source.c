a545d60edabe6e5fb2b9efe8717899002cb32963
scientiest35 <ibomyakmc@hotmasdail.com>
2018-11-11 06:27:50 +0300

    denedik oldu

---
+++ floatDe/Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+	float pi=3.14;
+	int a;
+	printf("%d\n",(int)pi);
+	printf("%f\n",pi);
+	a = pi;
+	printf("%d\n",a);
+	a = 2.15 * 3;
+	pi = 2.15*3;
+	printf("%d %f",a,pi);
+
+
+
+
+}
